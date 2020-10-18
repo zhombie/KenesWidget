3a2911ff24ff7df8b3a68d770486bf5b763ec87c
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2020-10-18 20:13:12 +0500

    Query language parameter

--- Sources/KenesWidget/KenesWidget.swift
+++ Sources/KenesWidget/KenesWidget.swift
@@ -1,11 +1,12 @@
 import SafariServices
 
 public final class KenesWidet: SFSafariViewController {
-    private static let widgetPath = "/admin/widget?is_mobile=true"
-    public init?(_ url: URL) {
+    private static let widgetPath = "/admin/widget?is_mobile=true&lang="
+
+    public init?(_ url: URL, language: String) {
         guard let scheme = url.scheme,
             let host = url.host,
-            let widgetURL = URL(string: "\(scheme)://\(host)\(KenesWidet.widgetPath)") else {
+            let widgetURL = URL(string: "\(scheme)://\(host)\(KenesWidet.widgetPath)\(language)") else {
             return nil
         }
 
