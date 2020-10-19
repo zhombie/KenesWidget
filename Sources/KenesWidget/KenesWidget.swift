b2f58d4f1197bc3926816612365f9ae3bee08ccd
Kostiantyn Madiar <k.madiar@gmail.com>
2020-10-20 01:33:49 +0300

    added Language cases

--- Sources/KenesWidget/KenesWidget.swift
+++ Sources/KenesWidget/KenesWidget.swift
@@ -1,12 +1,16 @@
 import SafariServices
 
 public final class KenesWidet: SFSafariViewController {
+    public enum Language: String {
+        case ru
+        case kz
+    }
     private static let widgetPath = "/admin/widget?is_mobile=true&lang="
 
-    public init?(_ url: URL, language: String) {
+    public init?(_ url: URL, language: Language = .kz) {
         guard let scheme = url.scheme,
             let host = url.host,
-            let widgetURL = URL(string: "\(scheme)://\(host)\(KenesWidet.widgetPath)\(language)") else {
+            let widgetURL = URL(string: "\(scheme)://\(host)\(KenesWidet.widgetPath)\(language.rawValue)") else {
             return nil
         }
 
